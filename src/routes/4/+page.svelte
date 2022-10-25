ea8de57e1b031a1712a99f8c70cc344a7307cd8d
Matt Lehrer <lehrerm@gmail.com>
2022-10-25 13:53:26 +0200

    4

---
+++ src/routes/4/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import PresenterNotes from '$lib/PresenterNotes.svelte';
+</script>
+
+<div
+	class="flex min-h-screen flex-col justify-center bg-gray-700 px-8 text-6xl font-thin text-gray-50"
+>
+	<p>Ideas</p>
+	<p>as building blocks</p>
+</div>
+
+<PresenterNotes>
+	<p />
+</PresenterNotes>
