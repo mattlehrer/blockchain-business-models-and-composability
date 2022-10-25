b5bb5ec4f5cf1bb43038f8453c421bfbbe0090bb
Matt Lehrer <lehrerm@gmail.com>
2022-10-25 14:30:50 +0200

    6

---
+++ src/routes/6/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import PresenterNotes from '$lib/PresenterNotes.svelte';
+	import cuneiform from './cuneiform.jpeg';
+</script>
+
+<div class="flex min-h-screen justify-center bg-white">
+	<img class=" max-h-screen w-auto max-w-[75%] object-contain py-8" src={cuneiform} alt="" />
+</div>
+
+<PresenterNotes>
+	<p>
+		Invention of writing
+		https://www.ancientworldwonders.com/uploads/posts/_2012/Sumerian_ancient_writing/2.jpg
+	</p>
+</PresenterNotes>
