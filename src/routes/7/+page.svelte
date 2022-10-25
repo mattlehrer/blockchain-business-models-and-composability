74616244b54cf93cc0182bdafa60c464d33cd2a7
Matt Lehrer <lehrerm@gmail.com>
2022-10-25 15:15:36 +0200

    7

---
+++ src/routes/7/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	import PresenterNotes from '$lib/PresenterNotes.svelte';
+	import printing_press from './printing_press.webp';
+</script>
+
+<div class="flex min-h-screen justify-center bg-black">
+	<img class=" max-h-screen w-auto object-contain" src={printing_press} alt="" />
+</div>
+
+<PresenterNotes>
+	<p>
+		Speed of copying and distribution increases. Same idea with digitalization but the ability to
+		build on ideas is fundamentally the same whether information is distributed in printed form or
+		electronically.
+	</p>
+	<p>
+		Invention of the printing press
+		https://interestingengineering.com/innovation/the-invention-and-history-of-the-printing-press
+	</p>
+</PresenterNotes>
