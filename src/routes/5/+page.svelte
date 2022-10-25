fd98de86c3a72d2e95d0cf1ef62d27db1427e4e1
Matt Lehrer <lehrerm@gmail.com>
2022-10-25 14:31:07 +0200

    5 update

---
+++ src/routes/5/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import PresenterNotes from '$lib/PresenterNotes.svelte';
+	import oralHistory from './oral_history.jpeg';
+</script>
+
+<div class="flex min-h-screen flex-col justify-center bg-black">
+	<img src={oralHistory} alt="" />
+</div>
+
+<PresenterNotes>
+	<p>
+		Oral history -
+		https://s3.amazonaws.com/s3.timetoast.com/public/uploads/photo/10126443/image/950bdd29d17ea3697701057b1b12f59f
+	</p>
+</PresenterNotes>
