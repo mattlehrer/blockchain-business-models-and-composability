72895ed078328898ba3c2d17e30615e010b2b0e1
Matt Lehrer <lehrerm@gmail.com>
2022-10-25 15:26:45 +0200

    9

---
+++ src/routes/9/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+	import PresenterNotes from '$lib/PresenterNotes.svelte';
+	import gun_parts from './gun_parts.jpeg';
+</script>
+
+<div class="flex min-h-screen justify-center bg-black">
+	<img class="max-h-screen w-auto object-contain" src={gun_parts} alt="" />
+</div>
+
+<PresenterNotes>
+	<p>
+		End of the 18th, beginning of the 19th century, businesses can use these building blocks You
+		could manufacture one part, cheaper, or better, and sell it to the first company’s customers You
+		could use other parts to make something new
+	</p>
+	<p>You don’t have to build everything. It’s not just knowledge anymore.</p>
+	<p>
+		Interchangeable parts
+		https://s3.amazonaws.com/s3.timetoast.com/public/uploads/photos/9104682/PartsCol.jpg?1478142549
+	</p>
+</PresenterNotes>
